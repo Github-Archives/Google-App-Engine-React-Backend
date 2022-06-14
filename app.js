437f3f9c0705e7dd9a3cd9841b1db0eb031a6cbf
Github-Archives <drewreynerwilliams@gmail.com>
2022-06-14 03:42:58 -0400

    Integrate graphql

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql'); // middleware for graphql
+const { buildSchema } = require('graphql') // takes a string and returns a schema object
+
 
 const app = express();
 
 app.use(bodyParser.json()); // parse incoming json bodies
 
-app.get('/', (req, res) => {
-    console.log('Running on localhost:3000')
-    res.send('Hollow World!');
-});
+app.use(
+    '/graphql', // single endpoint. '/graphql' can be named anything
+    graphqlHTTP({ // middleware
+        schema: buildSchema(`
+            type RootQuery {
+                events:[String!]!
+            }
+
+            type RootMutation {
+                createEvent(name: String): String
+            }
+
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: { // Resolver: rootValue is the root object that will be used to resolve queries
+            events: () => {
+                return ['Romantic Cooking', 'Sailing', 'Hiking'];
+            },
+            createEvent: (args) => {
+                const eventName = args.name;
+                return eventName;
+            }
+        },
+        graphiql: true // enables the GraphiQL interface (optional)
+    })
+);
 
 app.listen(3000)
 
