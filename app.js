bb54a0ee981383047f0d5932b578a59825037290
Github-Archives <drewreynerwilliams@gmail.com>
2022-06-14 05:49:21 -0400

    Working mongodb

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const { graphqlHTTP } = require('express-graphql'); // middleware for graphql
 const { buildSchema } = require('graphql') // takes a string and returns a schema object
 const mongoose = require('mongoose');
 
+const Event = require('./models/event');// import our own Model
+
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json()); // parse incoming json bodies
 
@@ -52,29 +52,45 @@ app.use(
                 mutation: RootMutation
             }
         `),
+
         // Resolver
         rootValue: { // Resolver: rootValue is the root object that will be used to resolve queries
             events: () => { // resolver function
-                return events;
+                return Event.find() // allows us to find documents in this collection
+                    .then(events => {
+                        return events.map(event => {
+                            return { ...event._doc, _id: event.id};
+                    });
+                })
+                .catch(err => {
+                    throw err;
+                });
             },
             createEvent: (args) => { // resolver function
-                const event = {
-                    _id: Math.random().toString(),
+                const event = new Event({
                     title: args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price, // + converts string to number/float
-                    date: args.eventInput.date
-                };
-                console.log(args)
-                events.push(event); // update/add event to array
-                return event; // don't just push to events array but return it too
+                    date: new Date(args.eventInput.date)
+                }); // Event constructor created by our Mongoose model
+                return event
+                    .save() // save to mongoDB because of the Mongoose package
+                    .then(result => {
+                        console.log(result);
+                        // return result; // returns too much metadata
+                        return {...result._doc, _id: event.id}; // _doc provided by mongoose that leaves out extra metadata
+                    })
+                    .catch(err => {
+                        console.log('Save Error: ', err)
+                        throw err; // graphql will throw an error too
+                    });
             }
         },
         graphiql: true // enables the GraphiQL interface (optional)
     })
 );
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.t1ymu.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.t1ymu.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
     .then(() => {
         app.listen(3000)
     })
