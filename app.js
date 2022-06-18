be7f289eefa99dd0bf3af62770f5c72a7ab2d7a3
Github-Archives <drewreynerwilliams@gmail.com>
2022-06-17 23:33:06 -0400

    add booking and cancel booking added

--- app.js
+++ app.js
@@ -1,178 +1,22 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const { graphqlHTTP } = require("express-graphql") // middleware for graphql
-const { buildSchema } = require("graphql") // takes a string and returns a schema object
+// const { buildSchema } = require("graphql") // takes a string and returns a schema object
 const mongoose = require("mongoose") // relates to mongoDB
-const bcrypt = require("bcryptjs") // password hashing
 
-const Event = require("./models/event") // import our own event Model
-const User = require("./models/user") // import our own user Model
+const graphQlSchema = require("./graphql/schema/index")
+const graphQlResolvers = require("./graphql/resolvers/index")
 
 const app = express()
 
 app.use(bodyParser.json()) // parse incoming json bodies
 
-const events = (eventIds) => {
-  return Event.find({ _id: { $in: eventIds } })
-    .then((events) => {
-      return events.map((event) => {
-        return {
-          ...event._doc,
-          _id: event.id,
-          creator: user.bind(this, event.creator),
-        }
-      })
-    })
-    .catch((err) => {
-      throw err
-    })
-}
-
-// fetch a user by its id
-const user = (userId) => {
-  return User.findById(userId)
-    .then((user) => {
-      return {
-        ...user._doc,
-        _id: user.id,
-        createdEvents: events.bind(this, user._doc.createdEvents),
-      }
-    })
-    .catch((err) => {
-      throw err
-    })
-} //
-
-// name 'Event' anything
-// _id is a mongoDB id, ID is type
-// '!' means it is required/not nullable
-// this just cleans up what you're sending RootMutation.createEvent params
-// return an array of events !needs to be non-nullable/Event
-
 app.use(
   "/graphql", // single endpoint. '/graphql' can be named anything
   graphqlHTTP({
     // middleware
-    schema: buildSchema(`
-            type Event {
-                _id: ID!
-                title: String!
-                description: String!
-                price: Float!
-                date: String!
-                creator: User!
-            }
-
-            type User {
-                _id: ID!
-                email: String!
-                password: String
-                createdEvents: [Event!]
-            }
-
-            input EventInput {
-                title: String!
-                description: String!
-                price: Float!
-                date: String!
-            }
-
-            input UserInput {
-                email: String!
-                password: String!
-            }
-
-            type RootQuery {
-                events:[Event!]!
-            }
-
-            type RootMutation {
-                createEvent(eventInput: EventInput): Event
-                createUser(userInput: UserInput): User
-            }
-
-            schema {
-                query: RootQuery
-                mutation: RootMutation
-            }
-        `),
-
-    // Resolvers
-    rootValue: {
-      // Resolver: rootValue is the root object that will be used to resolve queries
-      events: () => {
-        // resolver function
-        return Event.find() // allows us to find documents in this collection
-          .then((events) => {
-            return events.map((event) => {
-              return {
-                ...event._doc,
-                _id: event.id,
-                creator: user.bind(this, event._doc.creator), // ?
-              }
-            })
-          })
-          .catch((err) => {
-            throw err
-          })
-      },
-      createEvent: (args) => {
-        // resolver function
-        const event = new Event({
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price, // + converts string to number/float
-          date: new Date(args.eventInput.date),
-          creator: "62a9348fa3bb6396bf534734", // new user. mongoose will automatically convert this string to a mongoDB id
-        }) // Event constructor created by our Mongoose model
-        let createdEvent // ?
-        return event
-          .save() // save to mongoDB because of the Mongoose package
-          .then((result) => {
-            // alternative -> return result; // returns too much metadata
-            createdEvent = { ...result._doc, _id: result._doc._id.toString() } // _doc provided by mongoose that leaves out extra metadata
-            return User.findById("62a9348fa3bb6396bf534734") //
-          })
-          .then((user) => {
-            if (!user) {
-              throw new Error("User not found")
-            }
-            user.createdEvents.push(event) // can pass event or id
-            return user.save()
-          })
-          .then((result) => {
-            return createdEvent
-          })
-          .catch((err) => {
-            console.log("Save Error: ", err)
-            throw err // graphql will throw an error too
-          })
-      },
-      createUser: (args) => {
-        // before creating a new user, make sure the email is not already in use in the db
-        return User.findOne({ email: args.userInput.email })
-          .then((user) => {
-            if (user) {
-              throw new Error("User already exists") // ! might need to delete this .then() statement
-            }
-            return bcrypt.hash(args.userInput.password, 12) // (hash arg sent, salting rounds)
-          })
-          .then((hashedPassword) => {
-            const user = new User({
-              email: args.userInput.email,
-              password: hashedPassword, // this would be stored as a plain text password in the database. if someone gets access to db they will see all passwords. so we need to use a hash instead
-            })
-            return user.save() // save to the db
-          })
-          .then((result) => {
-            // this is now the created user
-            return { ...result._doc, password: null, _id: result.id } // password: null so that mutation doesn't return password
-          })
-          .catch((err) => {
-            throw err
-          })
-      },
-    },
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
     graphiql: true, // enables the GraphiQL interface (optional)
   })
 )
---
+++ graphql/resolvers/index.js
@@ -0,0 +1,177 @@
+const bcrypt = require("bcryptjs") // password hashing
+
+const Event = require("../../models/event") // import our own event Model
+const User = require("../../models/user") // import our own user Model
+const Booking = require("../../models/booking")
+
+// name 'Event' anything
+// _id is a mongoDB id, ID is type
+// '!' means it is required/not nullable
+// this just cleans up what you're sending RootMutation.createEvent params
+// return an array of events !needs to be non-nullable/Event
+const events = async (eventIds) => {
+  try {
+    const events = await Event.find({ _id: { $in: eventIds } })
+    events.map((event) => {
+      return {
+        ...event._doc,
+        _id: event.id,
+        date: new Date(event._doc.date).toISOString(),
+        creator: user.bind(this, event.creator),
+      }
+    })
+    // ! Video states this line below should return events.map(...) to return the transformed events... but doesn't show this on github
+    return events
+  } catch (err) {
+    throw err
+  }
+}
+
+const singleEvent = async (eventId) => {
+  try {
+    const event = await Event.findById(eventId)
+    return {
+      ...event._doc,
+      _id: event.id,
+      creator: user.bind(this, event.creator),
+    }
+  } catch (err) {
+    throw err
+  }
+}
+
+// fetch a user by its id
+const user = async (userId) => {
+  try {
+    const user = await User.findById(userId)
+    return {
+      ...user._doc,
+      _id: user.id,
+      createdEvents: events.bind(this, user._doc.createdEvents),
+    }
+  } catch (err) {
+    throw err
+  }
+}
+
+module.exports = {
+  // Resolver: rootValue is the root object that will be used to resolve queries
+  events: async () => {
+    // resolver function
+    try {
+      const events = await Event.find() // allows us to find documents in this collection
+      return events.map((event) => {
+        return {
+          ...event._doc,
+          _id: event.id,
+          date: new Date(event._doc.date).toISOString(), // makes sure date string comes back more readable
+          creator: user.bind(this, event._doc.creator), // ?
+        }
+      })
+    } catch (err) {
+      throw err
+    }
+  },
+  bookings: async () => {
+    try {
+      const bookings = await Booking.find()
+      return bookings.map((booking) => {
+        return {
+          ...booking._doc,
+          _id: booking.id,
+          user: user.bind(this, booking._doc.user),
+          event: singleEvent.bind(this, booking._doc.event),
+          createdAt: new Date(booking._doc.createdAt).toISOString(),
+          updatedAt: new Date(booking._doc.updatedAt).toISOString(),
+        }
+      })
+    } catch (err) {
+      throw err
+    }
+  },
+  createEvent: async (args) => {
+    // resolver function
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price, // + converts string to number/float
+      date: new Date(args.eventInput.date),
+      // ! this number is hardcoded rn from events/_id: ObjectId('here')
+      creator: "62ad36872f5a4044773b8026", // new user. mongoose will automatically convert this string to a mongoDB id
+    }) // Event constructor created by our Mongoose model
+    let createdEvent // ?
+    try {
+      const result = await event.save() // save to mongoDB because of the Mongoose package
+      createdEvent = {
+        ...result._doc,
+        _id: result._doc._id.toString(),
+        date: new Date(event._doc.date).toISOString(), // makes sure date string comes back more readable
+        creator: user.bind(this, result._doc.creator),
+      } // _doc provided by mongoose that leaves out extra metadata
+      // ! this SAME number is hardcoded rn from events/_id: ObjectId('here')
+      const creator = await User.findById("62ad36872f5a4044773b8026")
+
+      if (!creator) {
+        throw new Error("User not found.")
+      }
+      creator.createdEvents.push(event) // can pass event or id
+      await creator.save()
+
+      return createdEvent
+    } catch (err) {
+      console.log(err)
+      throw err
+    }
+  },
+  createUser: async (args) => {
+    try {
+      // before creating a new user, make sure the email is not already in use in the db
+      const existingUser = await User.findOne({ email: args.userInput.email })
+      if (existingUser) {
+        throw new Error("User exists already.")
+      }
+      const hashedPassword = await bcrypt.hash(args.userInput.password, 12) // (hash arg sent, salting rounds)
+
+      const user = new User({
+        email: args.userInput.email,
+        password: hashedPassword, // this would be stored as a plain text password in the database. if someone gets access to db they will see all passwords. so we need to use a hash instead
+      })
+
+      const result = await user.save() // save to the db
+      // this is now the created user
+      return { ...result._doc, password: null, _id: result.id } // password: null so that mutation doesn't return password
+    } catch (err) {
+      throw err
+    }
+  },
+  bookEvent: async (args) => {
+    const fetchedEvent = await Event.findOne({ _id: args.eventId })
+    const booking = new Booking({
+      user: "62ad36872f5a4044773b8026",
+      event: fetchedEvent,
+    })
+    const result = await booking.save()
+    return {
+      ...result._doc,
+      _id: result.id,
+      user: user.bind(this, booking._doc.user),
+      event: singleEvent.bind(this, booking._doc.event),
+      createdAt: new Date(result._doc.createdAt).toISOString(),
+      updatedAt: new Date(result._doc.updatedAt).toISOString(),
+    }
+  },
+  cancelBooking: async (args) => {
+    try {
+      const booking = await Booking.findById(args.bookingId).populate("event")
+      const event = {
+        ...booking.event._doc,
+        _id: booking.event.id,
+        creator: user.bind(this, booking.event._doc.creator),
+      }
+      await Booking.deleteOne({ _id: args.bookingId })
+      return event
+    } catch (err) {
+      throw err
+    }
+  },
+}
